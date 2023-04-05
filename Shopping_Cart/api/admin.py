60545e9b87605dbf16e4edc7d8bb199e3d072980
Victor Cifuentes <vdcifuentesg@uqvirtual.edu.co>
2023-04-05 10:40:15 -0500

    Creación y registro de los modelo de datos

--- Shopping_Cart/api/admin.py
+++ Shopping_Cart/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Producto, CarritoDeCompras
 
 # Register your models here.
+@admin.register(CarritoDeCompras)
+class CarritoDeComprasAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    pass
--- Shopping_Cart/api/models.py
+++ Shopping_Cart/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=3)
+    categoria = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+class CarritoDeCompras(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.IntegerField()
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, to_field="id")
+    cantidad = models.IntegerField(default=1)
